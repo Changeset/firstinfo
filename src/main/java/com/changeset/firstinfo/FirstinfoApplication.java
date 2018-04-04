7da8ee2575c0cd699082b423815517a3ead2f43f
Changeset <695700827@qq.com>
2018-04-04 14:57:11 +0800

    update spring boot source code for Java Web.

---
+++ src/main/java/com/changeset/firstinfo/FirstinfoApplication.java
@@ -0,0 +1,12 @@
+package com.changeset.firstinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstinfoApplication.class, args);
+	}
+}
