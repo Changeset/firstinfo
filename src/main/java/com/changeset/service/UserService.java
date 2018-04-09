e304d36324f6375e6bea6d7869ae7d971a9bf21c
Changeset <695700827@qq.com>
2018-04-09 21:04:43 +0800

    update News representing

--- src/main/java/com/changeset/service/UserService.java
+++ src/main/java/com/changeset/service/UserService.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.util.*;
 
 
